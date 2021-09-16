5ef34e89a2f444f3984504848e23be9759c2323c
jesseplamondon <jesse.plamondon57@gmail.com>
2021-09-15 20:59:53 -0700

    Video commit 2

--- src/main.java
+++ src/main.java
@@ -20,7 +20,7 @@ public class main {
 		}
 		return sum;
 	}
-	//Greatest Increasing Subset Feature
+	//Greatest Increasing Subset Feature <edit>
 	public static ArrayList <Integer> increasingSubset(int [] A) {
 		int maxSum = 0;
 		ArrayList <Integer> max = new ArrayList<Integer>(Arrays.asList());
